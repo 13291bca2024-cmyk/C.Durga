9cf09f3d86ab8196ac2e732975f3fcb787c4268d
HARITHA V <13291bca2024@princescience.in>
2025-09-03 15:05:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Highlight active menu item
+const links = document.querySelectorAll("nav a");
+
+links.forEach(link => {
+  link.addEventListener("click", () => {
+    links.forEach(item => item.classList.remove("active"));
+    link.classList.add("active");
+  });
+});
